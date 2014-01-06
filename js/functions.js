fcb6ae33a217a96f7712dc1f8be166be6a2eaca4
John <juan@losmaquiladores.com>
2014-01-06 17:12:44 -0600

    Reload on data null

--- js/functions.js
+++ js/functions.js
@@ -8,16 +8,14 @@
 		$('.not_round').buttonMarkup({ corners: false });
 
 		$(document).ready(function(){
-			setTimeout(function(){
+			
 				$('.cycle-gallery').cycle({
 					slides : 'li',
 					fx     : 'scrollHorz',
 					pager  : '.cycle-pager',
 					swipe  : true
 				});
-				// console.log('lol');
-			}, 1000);
-			
+
 
 			/// FEED ACTIVIDADES //////////////////////////////
 
