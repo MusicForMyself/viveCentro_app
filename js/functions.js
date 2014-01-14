b87ac5609017b13ae7d8a8362f7fea4f0bf6d46d
John <juan@losmaquiladores.com>
2014-01-14 11:52:08 -0600

    Mapa del recorrido

--- js/functions.js
+++ js/functions.js
@@ -19,18 +19,38 @@
 
 			/// FEED ACTIVIDADES //////////////////////////////
 
-			$('.menu_trigger').on('click', function(){
-				if( $('.ui-panel-closed').length > 0 ){
-					// Mover boton del menu
-					$('.menu_trigger').addClass('open');
-					return;
-				}
+			// $('.menu_trigger').on('click', function(){
+			// 	if( $('.ui-panel-closed').length > 0 ){
+			// 		// Mover boton del menu
+			// 		$('.menu_trigger').addClass('open');
+			// 		return;
+			// 	}
+			// 	$('.menu_trigger').removeClass('open');
+			// });
+
+			$('#menu_panel').on('panelbeforeopen', function(e,u){
+				e.preventDefault();
+				$('.menu_trigger').addClass('open');
+				$('.the_header').css('position', 'relative');
+				console.log('lol catz');
+			})
+			.on('panelbeforeclose', function(e,u){
+				e.preventDefault();
 				$('.menu_trigger').removeClass('open');
+				console.log('lol catz close');
 			});
+
+
 			
 			$('.share').on('click', function(){
 				//Desplegar el pop de share
+				( !$('.submenu:visible').length > 0 ) ? $('.share').css('background-color' , '#000') : $('.share').css('background-color' , '#EAEAEA');
+				$(this).find('.submenu').toggle('fast');
+				
 			});
+			
+			var windowHeight =  $(window).height();
+			$('#map-canvas').css('height', windowHeight-120);
 
 
 		});
