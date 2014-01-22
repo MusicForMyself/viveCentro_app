eeeb59fcd39597de673dac2e160ce1fd77c3ef7b
John <juan@losmaquiladores.com>
2014-01-21 21:19:19 -0600

    Set favorites

---
+++ js/facebook.js
@@ -0,0 +1,212 @@
+/**************************************************
+	  __                _                 _
+	 / _| __ _  ___ ___| |__   ___   ___ | | __
+	| |_ / _` |/ __/ _ \ '_ \ / _ \ / _ \| |/ /
+	|  _| (_| | (_|  __/ |_) | (_) | (_) |   <
+	|_|  \__,_|\___\___|_.__/ \___/ \___/|_|\_\
+
+ **************************************************/
+
+
+
+(function($){
+
+	"use strict";
+
+	$(function(){
+
+
+		window.vivecentroFB = {
+			Settings: {
+				appId: '348321661974739',
+				oauth : true
+			},
+			Scope: { scope: 'email, publish_actions' },
+			Friends: {},
+			MoreFirends: [],
+			countFriends: 0
+		};
+
+
+
+	// INTEGRACION API FACEBOOK //////////////////////////////////////////////////////////
+
+
+
+		$.ajaxSetup({ cache: true }); // Set default values for future Ajax requests.
+
+
+		vivecentroFB.loginCallback = function (response) {
+			if (response.status === 'connected') {
+
+				$('body').removeClass('no-facebook');
+				vivecentroFB.save_login_data();
+			} else if (response.status == 'not_authorized') {
+				
+				$('body').addClass('no-facebook');
+				vivecentroFB.save_not_logged();
+
+			}
+		};
+
+
+		vivecentroFB.loginFacebookUser = function (){
+			FB.login( vivecentroFB.loginCallback, vivecentroFB.Scope );
+		};
+
+
+		vivecentroFB.getUserProfilePicture = function (friend) {
+
+
+			FB.api("/"+friend.facebook_id+"/?fields=name,username,picture",  function (response) {
+
+				var userName     = response.name,
+					profileUrl   = 'https://www.facebook.com/'+response.username,
+					coverPicture = response.picture.data.url;
+
+				var link = $('<a></a>',{
+					href: profileUrl,
+					title: userName,
+					target: '_blank'
+				});
+
+				$('<img />',{
+					src: coverPicture,
+					class: 'user'
+				}).appendTo(link);
+
+				link.appendTo('.usuarios');
+
+			});
+		};
+
+		// Functions
+		vivecentroFB.save_login_data = function () {
+			FB.api('/me', function(response) {
+				var storage = window.localStorage;
+				var user_ID = response.id;
+				storage.setItem('fb_login_id', user_ID );
+				
+				console.log('Good to see you, ' + response.name + '.');
+			});
+		}
+
+		vivecentroFB.save_not_logged = function(){
+			var storage = window.localStorage;
+			var user_ID = 'not_logged';
+			storage.setItem('fb_login_id', user_ID );
+		}
+
+
+		vivecentroFB.addHiddenFriend = function (friend) {
+			FB.api("/"+friend.facebook_id+"/?fields=name,username,picture",  function (response) {
+
+				var userName     = response.name,
+					profileUrl   = 'https://www.facebook.com/'+response.username,
+					coverPicture = response.picture.data.url;
+
+				vivecentroFB.MoreFirends.push({
+					name: userName,
+					url: profileUrl,
+					picture: coverPicture
+				});
+
+			});
+		};
+
+
+		vivecentroFB.checkFriendsPosts = function (friends, post_id){
+
+			$.get(ajax_url, {
+				friends: friends,
+				post_id: post_id,
+				action: 'get_friends_posts'
+			}, 'json')
+
+			.done(function (data) {
+
+				var json = JSON.parse(data);
+
+				if( json.length > 0){
+					$('.loading').show();
+					$('.leyeron').show();
+				}
+
+				$.each(json, function (index, friend) {
+					if ( ++vivecentroFB.countFriends > 5 ) {
+						$('#more_friends').show();
+						vivecentroFB.addHiddenFriend( friend );
+					} else {
+						vivecentroFB.getUserProfilePicture( friend );
+					}
+
+
+				});
+
+			});
+		};
+
+
+		vivecentroFB.showFiendsWhoReadThisPost = function (friends, post_id) {
+
+			var countFriends = friends.length;
+
+			if( countFriends > 100 ){
+				var friendChunks = json_split(friends, 100);
+				$.each(friendChunks, function (index, chunk) {
+					vivecentroFB.checkFriendsPosts(chunk, post_id);
+				});
+			} else {
+				vivecentroFB.checkFriendsPosts(friends, post_id);
+			}
+
+		};
+
+
+		vivecentroFB.getUserFriends = function(){
+			FB.api('/me/friends',function(response) {
+				if( ! response.data) {
+					return false;
+				}
+
+				vivecentroFB.Friends = $.map(response.data, function (friend, index) {
+					return friend.id;
+				});
+				vivecentroFB.showFiendsWhoReadThisPost(
+					vivecentroFB.Friends,
+					$('.titulo_single').data('post_id')
+				);
+			});
+		};
+
+
+		vivecentroFB.getLoginStatusCallback = function (response){
+			if (response.status === 'connected') { // mostrar contenido exclusivo para usuarios que autorizaron
+
+				$('body').removeClass('no-facebook');
+			} else {
+				vivecentroFB.loginFacebookUser();
+				$('body').addClass('no-facebook');
+			}
+		};
+
+
+		vivecentroFB.init = function () {
+			$.getScript(
+				'https://connect.facebook.net/es_ES/all.js'
+			).done(function () {
+				FB.init( vivecentroFB.Settings );
+				FB.getLoginStatus( vivecentroFB.getLoginStatusCallback );
+			});
+		};
+
+
+		$(document).ready(function(){
+			vivecentroFB.init();
+		});
+
+		
+
+	});
+
+})(jQuery);
--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,10 @@
 	"use strict";
 
 	$(function(){
+		// Check if user has set a login option
+		if( typeof(on_index)!== 'undefined' && window.localStorage.getItem('fb_login_id') ){
+			goHome();
+		}
 
 
 		$('.not_round').buttonMarkup({ corners: false });
@@ -15,30 +19,19 @@
 					pager  : '.cycle-pager',
 					swipe  : true
 				});
-
-
-			/// FEED ACTIVIDADES //////////////////////////////
-
-			// $('.menu_trigger').on('click', function(){
-			// 	if( $('.ui-panel-closed').length > 0 ){
-			// 		// Mover boton del menu
-			// 		$('.menu_trigger').addClass('open');
-			// 		return;
-			// 	}
-			// 	$('.menu_trigger').removeClass('open');
-			// });
+				
 
 			$('#menu_panel').on('panelbeforeopen', function(e,u){
 				e.preventDefault();
 				$('.menu_trigger').addClass('open');
 				$('.the_header').css('position', 'relative');
-				console.log('lol catz');
+				
 			})
 			.on('panelbeforeclose', function(e,u){
 				e.preventDefault();
 				$('.menu_trigger').removeClass('open');
 				$('.the_header').css('position', 'fixed');
-				console.log('lol catz close');
+				
 			});
 
 
@@ -53,10 +46,80 @@
 			var windowHeight =  $(window).height();
 			$('#map-canvas').css('height', windowHeight-120);
 
+			// Login with facebook events
+			$('#login_fb').on('click', function(){
+				vivecentroFB.loginFacebookUser();
+				goHome();
+			});
+
+			$('#not_login_fb').on('click', function(){
+				vivecentroFB.save_not_logged();
+				goHome();
+			});
+
+			// Favorite stuff
+			$('.favorite').on('click', function(){
+
+				if ( checkFBuser() ){
+					var user_id   = window.localStorage.getItem('fb_login_id');
+					var this_post = window.localStorage.getItem('this_post_id');
+
+					console.log(user_id);
+					console.log(this_post);
+					var result = postFavorite( user_id, this_post );
+
+					result.done(function (response){
+						if( response[0] == 'true' ){
+							navigator.notification.alert('Agregado a favoritos');
+							return;
+						}
+						navigator.notification.alert('Ya has agregado esta entrada a favoritos');
+						return;
+					});
+					return;
+				}
+				navigator.notification.alert('Necesitas estas conectado a Facebook para agregar a favoritos');
+				
+			});
 
 		});
 
 
 	});
+
+	window.getUrlVars = function() {
+	    var vars = {};
+	    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+	        vars[key] = value;
+	    });
+	    return vars;
+	}
+	
+	window.goHome = function(){
+		window.location = 'home.html';
+	}
+
+	window.checkFBuser = function (){
+		var storage = window.localStorage;
+		var user_id = storage.getItem('fb_login_id');
+
+		if( user_id !== 'not_logged' || typeof( user_id ) !== 'undefined' ){
+			return true;
+		}
+		vivecentroFB.loginFacebookUser();
+	}
+
+	window.postFavorite = function( user_id, a_post ){
+
+		console.log('user: '+ user_id + 'wants to favorite post: '+ a_post );
+
+		return $.getJSON('http://viveelcentro.mx/agregar-favorito/?user_id='+user_id+'&post_id='+ a_post, function(data){
+				if( !data ) location.reload();
+				console.log(data);
+			
+		});
+	
+	}
+	
 
 })(jQuery);
